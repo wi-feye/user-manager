ecf68eebe6dd286e0352dc117c3622709ec609b5
Geremia Pompei <geremiapompei@gmail.com>
2022-11-25 09:14:46 +0100

    update user info

--- src/init_static_db.py
+++ src/init_static_db.py
@@ -5,10 +5,10 @@ from src.models.User import User
 # create default user
 def init_user():
     user = User(
-        email="daniele.atzeni@phd.unipi.it",
-        name="Daniele",
-        surname="Atzeni",
-        password="password",
+        email="gustavo.milan@email.com",
+        name="Gustavo",
+        surname="Milan",
+        password="gustavo1",
         apikey_zerynth="G9froN8D4R.cF1znVzGvCejjc5BrzCsSqcqMaANPgRmFXMglCAWhkYttQFTymThnrf1ta7OQVP4",
         id_zerynth="acc-6sdna7wlhpno",
     )  
